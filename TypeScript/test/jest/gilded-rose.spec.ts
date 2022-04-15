a9d3e8ff637b84513fdab7c70df2a2a3a10b3a07
KrisJohnson <krazyk786@gmail.com>
2022-04-15 09:36:53 -0500

    test for quality below 1

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,11 +1,23 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const expected = 'foo';
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    const item = items[0];
-    expect(item.name).toBe(expected);
+  describe('updateQuality', () => {
+    it('should not change the item name', () => {
+      const expected = 'foo';
+      const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+      const items = gildedRose.updateQuality();
+      const item = items[0];
+      expect(item.name).toBe(expected);
+    });
+
+    describe('for regular items', () => {
+      it('should not lower the item quality if quality is less than 0', () => {
+        const expected = 0;
+        const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+        const items = gildedRose.updateQuality();
+        const item = items[0];
+        expect(item.quality).toBe(expected);
+      });
+    });
   });
 });
