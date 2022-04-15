904cf0c65d8132a17e7451f2ff976d45166c39d5
KrisJohnson <krazyk786@gmail.com>
2022-04-15 09:29:47 -0500

    passing name test

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -2,8 +2,10 @@ import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
   it('should foo', () => {
+    const expected = 'foo';
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    const item = items[0];
+    expect(item.name).toBe(expected);
   });
 });
