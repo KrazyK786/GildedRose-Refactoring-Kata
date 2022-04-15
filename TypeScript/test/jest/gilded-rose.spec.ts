504963358295f9fba1d490d7d1817c623a1db57c
KrisJohnson <krazyk786@gmail.com>
2022-04-15 14:22:46 -0500

    test for raising aged brie quality

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -11,7 +11,7 @@ describe('Gilded Rose', () => {
     });
 
     describe('for regular items', () => {
-      it('should not lower the item quality if quality is less than 0', () => {
+      it('should not lower the item quality if quality is less than 1', () => {
         const expected = 0;
         const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
         const items = gildedRose.updateQuality();
@@ -32,12 +32,22 @@ describe('Gilded Rose', () => {
         const item = items[0];
         expect(item.quality).toBe(expected);
       });
-      it('should lower the item sellIn if greter than 0', () => {
+      it('should lower the item sellIn if greater than 0', () => {
         const expected = 1;
         const gildedRose = new GildedRose([new Item('foo', 2, 2)]);
         const items = gildedRose.updateQuality();
         const item = items[0];
         expect(item.sellIn).toBe(expected);
+      });
+    });
+    
+    describe('for Aged Brie', () => {
+      it('should raise the quality if quality is less than 50', () => {
+        const expected = 3;
+        const gildedRose = new GildedRose([new Item('Aged Brie', 2, 2)]);
+        const items = gildedRose.updateQuality();
+        const item = items[0];
+        expect(item.quality).toBe(expected);
       });
     });
   });
