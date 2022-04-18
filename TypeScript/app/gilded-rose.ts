ef21fde056da75205b7e00eeb485c6fbfccd89da
KrisJohnson <krazyk786@gmail.com>
2022-04-17 19:23:14 -0500

    lowerQuality refactor

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,10 +1,13 @@
-const lowerQuality = (item: Item): Item => ({ ...item, quality: item.quality - 1 });
+const pipe = (...functions) => x => functions.reduce((acc, fn) => fn(acc), x);
+
+const lowerQuality = (item: Item): Item => !isSulfuras(item) ? ({ ...item, quality: item.quality - 1 }) : { ...item };
 const raiseQuality = (item: Item): Item => ({ ...item, quality: item.quality + 1 });
 const lowerSellIn = (item: Item): Item => ({ ...item, sellIn: item.sellIn - 1 });
 const qualityCanBeRaised = (item: Item): boolean => item.quality < 50;
-const isAgedBrie = (name: string): boolean => name === 'Aged Brie';
-const isBackstagePasses = (name: string): boolean => name === 'Backstage passes to a TAFKAL80ETC concert';
-const isSulfuras = (name: string): boolean => name === 'Sulfuras, Hand of Ragnaros';
+const qualityCanBeLowered = (item: Item): boolean => item.quality > 0;
+const isAgedBrie = (item: Item): boolean => item.name === 'Aged Brie';
+const isBackstagePasses = (item: Item): boolean => item.name === 'Backstage passes to a TAFKAL80ETC concert';
+const isSulfuras = (item: Item): boolean => item.name === 'Sulfuras, Hand of Ragnaros';
 
 export class Item {
   name: string;
@@ -27,16 +30,15 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (!isAgedBrie(this.items[i].name) && !isBackstagePasses(this.items[i].name)) {
-        if (this.items[i].quality > 0) {
-          if (!isSulfuras(this.items[i].name)) {
-            this.items[i] = lowerQuality(this.items[i]);
-          }
+      if (!isAgedBrie(this.items[i]) && !isBackstagePasses(this.items[i])) {
+        if (qualityCanBeLowered(this.items[i])) {
+          this.items[i] = lowerQuality(this.items[i]);
         }
-      } else {
+      } 
+      else {
         if (qualityCanBeRaised(this.items[i])) {
           this.items[i] = raiseQuality(this.items[i]);
-          if (isBackstagePasses(this.items[i].name)) {
+          if (isBackstagePasses(this.items[i])) {
             if (this.items[i].sellIn < 11) {
               if (qualityCanBeRaised(this.items[i])) {
                 this.items[i] = raiseQuality(this.items[i]);
@@ -50,21 +52,22 @@ export class GildedRose {
           }
         }
       }
-      if (!isSulfuras(this.items[i].name)) {
+
+      if (!isSulfuras(this.items[i])) {
         this.items[i] = lowerSellIn(this.items[i]);
       }
+
       if (this.items[i].sellIn < 0) {
-        if (!isAgedBrie(this.items[i].name)) {
-          if (!isBackstagePasses(this.items[i].name)) {
-            if (this.items[i].quality > 0) {
-              if (!isSulfuras(this.items[i].name)) {
-                this.items[i] = lowerQuality(this.items[i]);
-              }
+        if (!isAgedBrie(this.items[i])) {
+          if (!isBackstagePasses(this.items[i])) {
+            if (qualityCanBeLowered(this.items[i])) {
+              this.items[i] = lowerQuality(this.items[i]);
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
-        } else {
+        } 
+        else {
           if (qualityCanBeRaised(this.items[i])) {
             this.items[i] = raiseQuality(this.items[i]);
           }
