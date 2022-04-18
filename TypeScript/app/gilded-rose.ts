5f5a8933a331a8743a9432537be59ea986dee413
KrisJohnson <krazyk786@gmail.com>
2022-04-17 19:46:15 -0500

    refactor guard logic into functions

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,8 +1,8 @@
 const pipe = (...functions) => x => functions.reduce((acc, fn) => fn(acc), x);
 
-const lowerQuality = (item: Item): Item => !isSulfuras(item) ? ({ ...item, quality: item.quality - 1 }) : { ...item };
-const raiseQuality = (item: Item): Item => ({ ...item, quality: item.quality + 1 });
-const lowerSellIn = (item: Item): Item => ({ ...item, sellIn: item.sellIn - 1 });
+const lowerQuality = (item: Item): Item => !isSulfuras(item) && qualityCanBeLowered(item) ? ({ ...item, quality: item.quality - 1 }) : { ...item };
+const raiseQuality = (item: Item): Item => qualityCanBeRaised(item) ? ({ ...item, quality: item.quality + 1 }) : { ...item };
+const lowerSellIn = (item: Item): Item => !isSulfuras(item) ? ({ ...item, sellIn: item.sellIn - 1 }) : { ...item };
 const qualityCanBeRaised = (item: Item): boolean => item.quality < 50;
 const qualityCanBeLowered = (item: Item): boolean => item.quality > 0;
 const isAgedBrie = (item: Item): boolean => item.name === 'Aged Brie';
@@ -31,46 +31,32 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (!isAgedBrie(this.items[i]) && !isBackstagePasses(this.items[i])) {
-        if (qualityCanBeLowered(this.items[i])) {
-          this.items[i] = lowerQuality(this.items[i]);
-        }
+        this.items[i] = lowerQuality(this.items[i]);
       } 
       else {
-        if (qualityCanBeRaised(this.items[i])) {
-          this.items[i] = raiseQuality(this.items[i]);
-          if (isBackstagePasses(this.items[i])) {
-            if (this.items[i].sellIn < 11) {
-              if (qualityCanBeRaised(this.items[i])) {
-                this.items[i] = raiseQuality(this.items[i]);
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (qualityCanBeRaised(this.items[i])) {
-                this.items[i] = raiseQuality(this.items[i]);
-              }
-            }
+        this.items[i] = raiseQuality(this.items[i]);
+        if (isBackstagePasses(this.items[i])) {
+          if (this.items[i].sellIn < 11) {
+            this.items[i] = raiseQuality(this.items[i]);
+          }
+          if (this.items[i].sellIn < 6) {
+            this.items[i] = raiseQuality(this.items[i]);
           }
         }
       }
 
-      if (!isSulfuras(this.items[i])) {
-        this.items[i] = lowerSellIn(this.items[i]);
-      }
+      this.items[i] = lowerSellIn(this.items[i]);
 
       if (this.items[i].sellIn < 0) {
         if (!isAgedBrie(this.items[i])) {
           if (!isBackstagePasses(this.items[i])) {
-            if (qualityCanBeLowered(this.items[i])) {
-              this.items[i] = lowerQuality(this.items[i]);
-            }
+            this.items[i] = lowerQuality(this.items[i]);
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } 
         else {
-          if (qualityCanBeRaised(this.items[i])) {
-            this.items[i] = raiseQuality(this.items[i]);
-          }
+          this.items[i] = raiseQuality(this.items[i]);
         }
       }
     }
